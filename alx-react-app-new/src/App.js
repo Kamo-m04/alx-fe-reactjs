07f9ae4b1319a930b08bfad6d39caeb09ca3ce8a
KamogeloReese <kamogeloreese@gmail.com>
2025-09-11 23:57:02 +0200

    Add clean alx-react-app-new project

--- alx-react-app-new/src/App.js
+++ alx-react-app-new/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import UserProfile from './components/UserProfile';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <UserProfile />
+      <MainContent />
+      <Counter />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header style={{ backgroundColor: '#333', padding: '15px', textAlign: 'center' }}>
+      <h1 style={{ color: 'white' }}>Welcome to My React App</h1>
+    </header>
+  );
+}
+
+export default Header;
